df503f2fa69374a0eb23be5670a6267077794406
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-18 14:28:49 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> fruitPrices = new HashMap<>();
+
+        fruitPrices.put("사과", 17980);
+        fruitPrices.put("배", 10320);
+        fruitPrices.put("수박", 15920);
+        fruitPrices.put("멜론", 10630);
+    }
+}
