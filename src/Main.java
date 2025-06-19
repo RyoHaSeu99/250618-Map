642c5b63da6c8df59be74bc4ffc0e7abb622532b
RyoHaSeu99 <cmrhanc123@naver.com>
2025-06-19 09:50:47 +0900

    entry.getKey + entry.getValue

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> fruitPrices = new HashMap<>();
+        Map<String, Integer> fruitPrices = new TreeMap<>();
 
         fruitPrices.put("사과", 17980);
         fruitPrices.put("배", 10320);
         fruitPrices.put("수박", 15920);
         fruitPrices.put("멜론", 10630);
 
-        fruitPrices.replace("사과", 100);
-        fruitPrices.remove("멜론");
-        for(Map.Entry<String, Integer> entry : fruitPrices.entrySet()) {
+        for(Map.Entry<String, Integer> entry: fruitPrices.entrySet()) {
             System.out.println(entry.getKey() + entry.getValue());
         }
-
-
     }
 }
